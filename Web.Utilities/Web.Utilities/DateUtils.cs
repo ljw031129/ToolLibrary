1c7f0fbb3a95d9be8f021c5617a26930005a84f2
鲁建伟 <ljw031129@hotmail.com>
2015-02-02 14:47:36 +0800

    c1

--- Web.Utilities/Web.Utilities/DateUtils.cs
+++ Web.Utilities/Web.Utilities/DateUtils.cs
@@ -9,6 +9,41 @@ namespace Web.Utilities
     public static class DateUtils
     {
         /// <summary>
+        /// 将Unix时间戳转换为DateTime类型时间
+        /// </summary>
+        /// <param name="d">double 型数字</param>
+        /// <returns>DateTime</returns>
+        public static System.DateTime ConvertIntDateTime(double d)
+        {
+            System.DateTime time = System.DateTime.MinValue;
+            System.DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1));
+            time = startTime.AddMilliseconds(d);
+            return time;
+        }
+        /// <summary>
+        /// DateTime时间格式转换为Unix时间戳格式
+        /// </summary>
+        /// <param name="time"> DateTime时间格式</param>
+        /// <returns>Unix时间戳格式</returns>
+        public static int ConvertDateTimeIntInt(System.DateTime time)
+        {
+            System.DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1));
+            return (int)(time - startTime).TotalSeconds;
+        }
+        /// <summary>
+        /// 将c# DateTime时间格式转换为Unix时间戳格式
+        /// </summary>
+        /// <param name="time">时间</param>
+        /// <returns>long</returns>
+        public static long ConvertDateTimeInt(System.DateTime time)
+        {
+            //double intResult = 0;
+            System.DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1, 0, 0, 0, 0));
+            //intResult = (time- startTime).TotalMilliseconds;
+            long t = (time.Ticks - startTime.Ticks) / 10000;            //除10000调整为13位
+            return t;
+        }
+        /// <summary>
         /// Creates a date format of dd MMMM yyyu
         /// </summary>
         /// <param name="theDate"></param>
